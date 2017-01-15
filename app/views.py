6c8fc141423f14f66251652babe9573b7998126f
Alex Crist <accrist@gmail.com>
2017-01-15 03:20:24 -0700

    format changes

--- app/views.py
+++ app/views.py
@@ -8,20 +8,21 @@ from .forms import LoginForm
 def index():
     user = {'nickname': 'Miguel'}  # fake user
     posts = [  # fake array of posts
-        { 
-            'author': {'nickname': 'John'}, 
-            'body': 'Beautiful day in Portland!' 
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
         },
-        { 
-            'author': {'nickname': 'Susan'}, 
-            'body': 'The Avengers movie was so cool!' 
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
         }
     ]
     return render_template("index.html",
                            title='Home',
                            user=user,
                            posts=posts,
-						   )
+                           )
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -30,7 +31,7 @@ def login():
         flash('Login requested for OpenID="%s", remember_me=%s' %
               (form.openid.data, str(form.remember_me.data)))
         return redirect('/index')
-    return render_template('login.html', 
+    return render_template('login.html',
                            title='Sign In',
-                           form=form)
-						   
+                           form=form,
+                           )
